542236dcf94c5ee3511c1b48c2a7b5bf83e778bd
bdossantos <bdossantos@clever-age.con>
2024-01-04 20:21:09 +0100

    add varriable env google = install bundle google/apiclient

--- public/js/dateTime.js
+++ public/js/dateTime.js
@@ -1,6 +1,13 @@
 function displayDateTime() {
     const currentDate = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', /*hour: '2-digit', minute: '2-digit', second: '2-digit'*/ };
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        /*hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'*/ };
     document.getElementById('currentTime').innerText = currentDate.toLocaleString('fr-FR', options);
 }
 
